109af3fca09974ebc532dbe2989e3bae44db50e4
antonivegaochoa <tonivegaochoa@gmail.com>
2021-08-11 17:27:18 -0500

    Initial Commit

---
+++ Accessibility/AccessibilityApp.swift
@@ -0,0 +1,17 @@
+//
+//  AccessibilityApp.swift
+//  Accessibility
+//
+//  Created by Antonio Vega on 8/11/21.
+//
+
+import SwiftUI
+
+@main
+struct AccessibilityApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
